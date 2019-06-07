f61822619d68b43a3e357e6b0f5fa0b293cc095e
Sven Bretschneider <mail@svenbretschneider.de>
2019-06-07 16:41:16 +0200

    (feat): jurisdiction id unique for jurisdiction

--- imports/plugins/included/taxes-rates/server/no-meteor/util/taxesForItem.js
+++ imports/plugins/included/taxes-rates/server/no-meteor/util/taxesForItem.js
@@ -2,6 +2,32 @@ import { toFixed } from "accounting-js";
 import Random from "@reactioncommerce/random";
 
 /**
+ * Generates an ID unique to a jurisdiction
+ * @param {Object} taxDef The tax definition
+ * @returns {String} The jurisdictionId
+ */
+function getJurisdictionId(taxDef) {
+  const { country, region, postal } = taxDef;
+  let jurisdictionId = "";
+  if (country !== undefined && country !== null) {
+    jurisdictionId += country;
+  } else {
+    jurisdictionId += "*";
+  }
+  if (region !== undefined && region !== null) {
+    jurisdictionId += region;
+  } else {
+    jurisdictionId += "*";
+  }
+  if (postal !== undefined && postal !== null) {
+    jurisdictionId += postal;
+  } else {
+    jurisdictionId += "*";
+  }
+  return jurisdictionId;
+}
+
+/**
  * @param {Object[]} allTaxes Array of tax definition docs
  * @param {Number} amount The amount to be taxed
  * @param {String} taxCode Tax code applicable to the item
@@ -20,7 +46,7 @@ export default function taxesForItem(allTaxes, { amount, taxCode }) {
       return {
         _id: Random.id(),
         country: taxDef.country,
-        jurisdictionId: taxDef._id,
+        jurisdictionId: getJurisdictionId(taxDef),
         postal: taxDef.postal,
         region: taxDef.region,
         sourcing: taxDef.taxLocale,
